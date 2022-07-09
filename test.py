796e113f9fc694045b35ebca84e31c3d0b9d9f62
yuan <10863121@gm.nfu.edu.tw>
2022-07-09 12:47:33 +0800

    update test.py

--- test.py
+++ test.py
@@ -7,13 +7,14 @@ from utils.jpeg import JPEG
 from utils.metrics import cal_ssim, cal_ms_ssim, psnr
 import os
 import argparse
+from tqdm import tqdm
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-b", "--batch", type=int, default=8)
     parser.add_argument("-w", "--weight", type=str, default="model")
     parser.add_argument("-m", "--maps", type=int, default=96)
-    parser.add_argument("-l", "--lr", action="store_true")
+    parser.add_argument("-l", "--less", action="store_true")
     args = parser.parse_args()
     
     weight = args.weight
@@ -31,7 +32,15 @@ if __name__ == "__main__":
     #                         shuffle=True,
     #                         num_workers=8,
     #                         pin_memory=True)
-
+    
+    path = "../dataset/vimeo90k"
+    test_txt = open(f"{path}/sep_testlist.txt", "r")
+    test_list = [i[:-1] for i in test_txt.readlines()]
+    if args.less:
+        test_list = test_list[:5]
+    lr_dir = "../dataset/vimeo90k/lr"
+    hr_dir = "../dataset/vimeo90k/hr"
+    log_file = open(f"result/{args.batch}_{args.maps}.csv", "w")
     model = SRX264(maps=args.maps)
 
     model.to(device)
@@ -39,19 +48,66 @@ if __name__ == "__main__":
     model.eval()
     # test_set = load_file("data", filename)
     # print(len(dctDataset))
-    target_frame = 1
-    lr_path = "../dataset/vimeo90k/lr/00002/0001"
-    f1_lr = (cv2.imread(f"{lr_path}/im{target_frame}.png").astype('float')/255).transpose(2, 0, 1)
-    f2_mv = (np.load(f"{lr_path}/mv{target_frame+1}.npy")).astype('float').transpose(2, 0, 1)
-    f2_lr = (cv2.imread(f"{lr_path}/im{target_frame+1}.png").astype('float')/255).transpose(2, 0, 1)
-    f3_mv = (np.load(f"{lr_path}/mv{target_frame+2}.npy")).astype('float').transpose(2, 0, 1)
-    f3_lr = (cv2.imread(f"{lr_path}/im{target_frame+2}.png").astype('float')/255).transpose(2, 0, 1)
-    lr = np.concatenate([f1_lr, f2_mv, f2_lr, f3_mv, f3_lr])
-    lr = torch.from_numpy(lr).to(device, dtype=torch.float)
-    lr = torch.unsqueeze(lr, 0)
-    opt = model(lr)
-    opt = opt[0].cpu().detach().numpy()*255
-    opt = opt.transpose(1, 2, 0)
-    cv2.imwrite(f"test1_{weight}.png", opt[:, :, :3])
-    cv2.imwrite(f"test2_{weight}.png", opt[:, :, 3:6])
-    cv2.imwrite(f"test3_{weight}.png", opt[:, :, 6:9])
+    log_file.write("target,frame,sr_psnr,sr_ssim,sr_ms_ssim\n")
+    for i in tqdm(test_list):
+        target_frames = [1, 4, 5]
+        lr_path = f"../dataset/vimeo90k/lr/{i}"
+        hr_path = f"../dataset/vimeo90k/hr/{i}"
+        log_file.write(f"{i}")
+        for target_frame in target_frames:
+            lr1 = cv2.imread(f"{lr_path}/im{target_frame}.png")
+            f1_lr = (lr1.astype('float')/255).transpose(2, 0, 1)
+            f2_mv = (np.load(f"{lr_path}/mv{target_frame+1}.npy")).astype('float').transpose(2, 0, 1)
+            lr2 = cv2.imread(f"{lr_path}/im{target_frame+1}.png")
+            f2_lr = (lr2.astype('float')/255).transpose(2, 0, 1)
+            f3_mv = (np.load(f"{lr_path}/mv{target_frame+2}.npy")).astype('float').transpose(2, 0, 1)
+            lr3 = cv2.imread(f"{lr_path}/im{target_frame+2}.png")
+            f3_lr = (lr3.astype('float')/255).transpose(2, 0, 1)
+            lr = np.concatenate([f1_lr, f2_mv, f2_lr, f3_mv, f3_lr])
+            lr = torch.from_numpy(lr).to(device, dtype=torch.float)
+            lr = torch.unsqueeze(lr, 0)
+            opt = model(lr)
+            opt = opt[0].cpu().detach().numpy()*255
+            opt = opt.transpose(1, 2, 0)
+            
+            # BGR
+            if target_frame != 5:
+                # lr1 = cv2.resize(lr1, (448, 256))
+                # lr2 = cv2.resize(lr2, (448, 256))
+                hr1 = cv2.imread(f"{hr_path}/im{target_frame}.png")
+                hr2 = cv2.imread(f"{hr_path}/im{target_frame+1}.png")
+                sr1 = np.round(opt[:, :, :3]).astype('uint8')
+                sr2 = np.round(opt[:, :, 3:6]).astype('uint8')
+                sr_psnr1 = np.mean(psnr(hr1, sr1))
+                sr_psnr2 = np.mean(psnr(hr2, sr2))
+                sr_ssim1 = cal_ssim(hr1, sr1)
+                sr_ssim2 = cal_ssim(hr2, sr2)
+                sr_ms_ssim1 = cal_ms_ssim(hr1, sr1)
+                sr_ms_ssim2 = cal_ms_ssim(hr2, sr2)
+                # lr_psnr1 = np.mean(psnr(hr1, lr1))
+                # lr_psnr2 = np.mean(psnr(hr2, lr2))
+                # lr_ssim1 = cal_ssim(hr1, lr1)
+                # lr_ssim2 = cal_ssim(hr2, lr2)
+                # lr_ms_ssim1 = cal_ms_ssim(hr1, lr1)
+                # lr_ms_ssim2 = cal_ms_ssim(hr2, lr2)
+                # log_file.write(f",im{target_frame+0},{lr_psnr1},{sr_psnr1},{lr_ssim1},{sr_ssim1},{lr_ms_ssim1},{sr_ms_ssim1}\n")
+                # log_file.write(f",im{target_frame+1},{lr_psnr2},{sr_psnr2},{lr_ssim2},{sr_ssim2},{lr_ms_ssim2},{sr_ms_ssim2}\n")
+                log_file.write(f",im{target_frame+0},{sr_psnr1},{sr_ssim1},{sr_ms_ssim1}\n")
+                log_file.write(f",im{target_frame+1},{sr_psnr2},{sr_ssim2},{sr_ms_ssim2}\n")
+                
+            # lr3 = cv2.resize(lr3, (448, 256))
+            hr3 = cv2.imread(f"{hr_path}/im{target_frame+2}.png")
+            sr3 = np.round(opt[:, :, 6:9]).astype('uint8')
+            sr_psnr3 = np.mean(psnr(hr3, sr3))
+            sr_ssim3 = cal_ssim(hr3, sr3)
+            sr_ms_ssim3 = cal_ms_ssim(hr3, sr3)
+            # lr_psnr3 = np.mean(psnr(hr3, lr3))
+            # lr_ssim3 = cal_ssim(hr3, lr3)
+            # lr_ms_ssim3 = cal_ms_ssim(hr3, lr3)
+            # log_file.write(f",im{target_frame+2},{lr_psnr3},{sr_psnr3},{lr_ssim3},{sr_ssim3},{lr_ms_ssim3},{sr_ms_ssim3}\n")
+            log_file.write(f",im{target_frame+2},{sr_psnr3},{sr_ssim3},{sr_ms_ssim3}\n")
+            
+            
+            
+    log_file.close()
+            
---
+++ test_bicubic.py
@@ -0,0 +1,38 @@
+import numpy as np
+import cv2
+from utils.metrics import cal_ssim, cal_ms_ssim, psnr
+import os
+import argparse
+from tqdm import tqdm
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--less", action="store_true")
+    args = parser.parse_args()
+    path = "../dataset/vimeo90k"
+    test_txt = open(f"{path}/sep_testlist.txt", "r")
+    test_list = [i[:-1] for i in test_txt.readlines()]
+    if args.less:
+        test_list = test_list[:5]
+    lr_dir = "../dataset/vimeo90k/lr"
+    hr_dir = "../dataset/vimeo90k/hr"
+    log_file = open(f"result/bicubic.csv", "w")
+    # test_set = load_file("data", filename)
+    # print(len(dctDataset))
+    log_file.write("target,frame,lr_psnr,lr_ssim,lr_ms_ssim\n")
+    for i in tqdm(test_list):
+        target_frames = [1,2,3,4,5,6,7]
+        lr_path = f"../dataset/vimeo90k/lr/{i}"
+        hr_path = f"../dataset/vimeo90k/hr/{i}"
+        log_file.write(f"{i}")
+        for target_frame in target_frames:
+            lr1 = cv2.imread(f"{lr_path}/im{target_frame}.png")
+            lr1 = cv2.resize(lr1, (448, 256))
+            hr1 = cv2.imread(f"{hr_path}/im{target_frame}.png")
+            lr_psnr1 = np.mean(psnr(hr1, lr1))
+            lr_ssim1 = cal_ssim(hr1, lr1)
+            lr_ms_ssim1 = cal_ms_ssim(hr1, lr1)            
+            log_file.write(f",im{target_frame},{lr_psnr1},{lr_ssim1},{lr_ms_ssim1}\n")
+
+    log_file.close()
+            
