d2364148c57b5305e19eddea8700bafa54ded952
HanYuan <lion70332@gmail.com>
2022-08-12 17:18:48 +0800

    update test_bicubic

--- test_bicubic.py
+++ test_bicubic.py
@@ -1,9 +1,13 @@
 import numpy as np
 import cv2
-from utils.metrics import convert_cal_ssim, convert_cal_ms_ssim, psnr
+from utils.metrics import cal_ssim, cal_ms_ssim, psnr
 import os
 import argparse
 from tqdm import tqdm
+# import lpips
+# import torch
+
+# loss_fn_alex = lpips.LPIPS(net='alex')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -13,12 +17,11 @@ if __name__ == "__main__":
     test_txt = open(f"{path}/sep_testlist.txt", "r")
     test_list = [i[:-1] for i in test_txt.readlines()]
     if args.less:
-        test_list = test_list[:5]
+        test_list = ["00002/0001"]
     lr_dir = "../dataset/vimeo90k/lr"
     hr_dir = "../dataset/vimeo90k/hr"
-    log_file = open(f"result/bicubic.csv", "w")
-    # test_set = load_file("data", filename)
-    # print(len(dctDataset))
+    log_file = open(f"show/bicubic.csv", "w")
+    
     log_file.write("target,frame,lr_psnr,lr_ssim,lr_ms_ssim\n")
     for i in tqdm(test_list):
         target_frames = [1,2,3,4,5,6,7]
@@ -27,12 +30,11 @@ if __name__ == "__main__":
         log_file.write(f"{i}")
         for target_frame in target_frames:
             lr1 = cv2.imread(f"{lr_path}/im{target_frame}.png")
-            lr1 = cv2.resize(lr1, (448, 256))
-            hr1 = cv2.imread(f"{hr_path}/im{target_frame}.png")
+            lr1 = cv2.resize(lr1, (448, 256))[:,:,::-1]
+            hr1 = cv2.imread(f"{hr_path}/im{target_frame}.png")[:,:,::-1]
             lr_psnr1 = np.mean(psnr(hr1, lr1))
-            lr_ssim1 = convert_cal_ssim(hr1, lr1)
-            lr_ms_ssim1 = convert_cal_ms_ssim(hr1, lr1)            
+            lr_ssim1 = cal_ssim(hr1, lr1)
+            lr_ms_ssim1 = cal_ms_ssim(hr1, lr1)            
             log_file.write(f",im{target_frame},{lr_psnr1},{lr_ssim1},{lr_ms_ssim1}\n")
 
     log_file.close()
-            
